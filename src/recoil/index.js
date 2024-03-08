fa8b6fdc5fa0fa447d43b49ecd02bb604012b482
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2024-03-08 15:20:56 +0900

    ;

--- src/recoil/index.js
+++ src/recoil/index.js
@@ -1,83 +1,83 @@
 "use client";
 import { useState, useEffect } from "react";
-import { RecoilRoot, atom, useRecoilState, useResetRecoilState } from "recoil";
 import { recoilPersist } from "recoil-persist";
+import { RecoilRoot, atom, useRecoilState, useResetRecoilState } from "recoil";
 import i18n from "@/locales/i18n";
 
 const { persistAtom } = recoilPersist();
 
 const defaultValue = {
-  config: { lang: "ko" },
-  user: {},
-  subject: {
-    // chartNumber
-    // date
-    // pre_g
-    // post_g
-    // pre_r
-    // post_r
-    // tab
-    // type
-  },
+    config: { lang: "ko" },
+    user: {},
+    subject: {
+        // chartNumber
+        // date
+        // pre_g
+        // post_g
+        // pre_r
+        // post_r
+        // tab
+        // type
+    },
 };
 
 const configState = atom({
-  key: "configState",
-  default: defaultValue.config,
-  effects_UNSTABLE: [persistAtom],
+    key: "configState",
+    default: defaultValue.config,
+    effects_UNSTABLE: [persistAtom],
 });
 
 const userState = atom({
-  key: "userState",
-  default: defaultValue.user,
-  effects_UNSTABLE: [persistAtom],
+    key: "userState",
+    default: defaultValue.user,
+    effects_UNSTABLE: [persistAtom],
 });
 
 const subjectState = atom({
-  key: "subjectState",
-  default: defaultValue.subject,
-  effects_UNSTABLE: [persistAtom],
+    key: "subjectState",
+    default: defaultValue.subject,
+    effects_UNSTABLE: [persistAtom],
 });
 
 export const useConfig = () => {
-  const [isInitial, setIsInitial] = useState(true);
-  const [config, setConfig] = useRecoilState(configState);
+    const [isInitial, setIsInitial] = useState(true);
+    const [config, setConfig] = useRecoilState(configState);
 
-  useEffect(() => {
-    setIsInitial(false);
-  }, []);
+    useEffect(() => {
+        setIsInitial(false);
+    }, []);
 
-  useEffect(() => {
-    i18n.changeLanguage(config.lang);
-  }, [config.lang]);
+    useEffect(() => {
+        i18n.changeLanguage(config.lang);
+    }, [config.lang]);
 
-  return [isInitial ? defaultValue.config : config, setConfig];
+    return [isInitial ? defaultValue.config : config, setConfig];
 };
 
 export const useUser = () => {
-  const [isInitial, setIsInitial] = useState(true);
-  const [user, setUser] = useRecoilState(userState);
-  const resetUser = useResetRecoilState(userState);
+    const [isInitial, setIsInitial] = useState(true);
+    const [user, setUser] = useRecoilState(userState);
+    const resetUser = useResetRecoilState(userState);
 
-  useEffect(() => {
-    setIsInitial(false);
-  }, []);
+    useEffect(() => {
+        setIsInitial(false);
+    }, []);
 
-  return [isInitial ? defaultValue.user : user, setUser, resetUser];
+    return [isInitial ? defaultValue.user : user, setUser, resetUser];
 };
 
 export const useSubject = () => {
-  const [isInitial, setIsInitial] = useState(true);
-  const [subject, setSubject] = useRecoilState(subjectState);
-  const resetSubject = useResetRecoilState(subjectState);
+    const [isInitial, setIsInitial] = useState(true);
+    const [subject, setSubject] = useRecoilState(subjectState);
+    const resetSubject = useResetRecoilState(subjectState);
 
-  useEffect(() => {
-    setIsInitial(false);
-  }, []);
+    useEffect(() => {
+        setIsInitial(false);
+    }, []);
 
-  return [isInitial ? defaultValue.subject : subject, setSubject, resetSubject];
+    return [isInitial ? defaultValue.subject : subject, setSubject, resetSubject];
 };
 
 export const Recoil = ({ children }) => {
-  return <RecoilRoot>{children}</RecoilRoot>;
+    return <RecoilRoot>{children}</RecoilRoot>;
 };
