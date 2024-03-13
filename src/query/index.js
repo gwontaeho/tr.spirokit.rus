fd6ccab61129dfbe53df987b46b669ac43e04fd9
taeho gwon <taeho8199@gmail.com>
2024-03-13 23:03:43 +0900

    ;

--- src/query/index.js
+++ src/query/index.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { setCookie, deleteCookie } from "cookies-next";
 import { axiosInstance, refreshHeader } from "@/apis";
@@ -10,8 +10,27 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 
+class Holder {
+  promise;
+  resolve;
+  reject;
+  constructor() {
+    this.hold();
+  }
+  hold() {
+    this.promise = new Promise((resolve, reject) =>
+      Object.assign(this, { reject, resolve })
+    );
+  }
+}
+
 export const QueryProvider = ({ children }) => {
   const router = useRouter();
+
+  const lock = useRef({
+    status: false,
+    holder: new Holder(),
+  });
 
   const [queryClient] = useState(
     () =>
@@ -22,6 +41,11 @@ export const QueryProvider = ({ children }) => {
         queryCache: new QueryCache({
           onError: async (error, query) => {
             if (error.response.status !== 401) return;
+
+            if (lock.current.status === true) {
+              await lock.current.holder.promise;
+              return query.fetch();
+            } else lock.current.status = true;
 
             try {
               const {
@@ -35,6 +59,10 @@ export const QueryProvider = ({ children }) => {
               );
               setCookie("accessToken", accessToken);
               error.config.headers.Authorization = `Bearer ${accessToken}`;
+
+              lock.current.holder.resolve();
+              lock.current.status = false;
+
               return query.fetch();
             } catch (error) {
               deleteCookie("accessToken");
