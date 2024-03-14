8d5742d03e48146da5716a7226451438242b0ac6
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2024-03-14 11:01:34 +0900

    ;

--- src/query/index.js
+++ src/query/index.js
@@ -4,77 +4,79 @@ import { useRouter } from "next/navigation";
 import { setCookie, deleteCookie } from "cookies-next";
 import { axiosInstance, refreshHeader } from "@/apis";
 
-import {
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 class Holder {
-  promise;
-  resolve;
-  reject;
-  constructor() {
-    this.hold();
-  }
-  hold() {
-    this.promise = new Promise((resolve, reject) =>
-      Object.assign(this, { reject, resolve })
-    );
-  }
-}
-
-export const QueryProvider = ({ children }) => {
-  const router = useRouter();
-
-  const lock = useRef({
-    status: false,
-    holder: new Holder(),
-  });
-
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: { refetchOnWindowFocus: false, retry: false },
-        },
-        queryCache: new QueryCache({
-          onError: async (error, query) => {
-            if (error.response.status !== 401) return;
-
-            if (lock.current.status === true) {
-              await lock.current.holder.promise;
-              return query.fetch();
-            } else lock.current.status = true;
-
-            try {
-              const {
-                data: {
-                  response: { accessToken },
+    isLocked = false;
+    promise;
+    reject;
+    resolve;
+    constructor() {
+        this.hold();
+    }
+    hold() {
+        this.promise = new Promise((resolve, reject) =>
+            Object.assign(this, {
+                reject: () => {
+                    this.isLocked = false;
+                    reject(this.hold());
                 },
-              } = await axiosInstance.post(
-                "/v1/auth/renewal",
-                null,
-                refreshHeader()
-              );
-              setCookie("accessToken", accessToken);
-              error.config.headers.Authorization = `Bearer ${accessToken}`;
+                resolve: () => {
+                    this.isLocked = false;
+                    resolve(this.hold());
+                },
+            })
+        );
+    }
+    lock() {
+        this.isLocked = true;
+    }
+}
+export const QueryProvider = ({ children }) => {
+    const router = useRouter();
 
-              lock.current.holder.resolve();
-              lock.current.status = false;
+    const holder = useRef(new Holder());
 
-              return query.fetch();
-            } catch (error) {
-              deleteCookie("accessToken");
-              deleteCookie("refreshToken");
-              router.replace("/signin");
-            }
-          },
-        }),
-      })
-  );
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: { refetchOnWindowFocus: false, retry: false },
+                },
+                queryCache: new QueryCache({
+                    onError: async (error, query) => {
+                        if (error.response.status !== 401) return;
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+                        if (holder.current.isLocked) {
+                            try {
+                                await holder.current.promise;
+                                return query.fetch();
+                            } catch (error) {
+                                return;
+                            }
+                        } else holder.current.lock();
+
+                        try {
+                            const {
+                                data: {
+                                    response: { accessToken },
+                                },
+                            } = await axiosInstance.post("/v1/auth/renewal", null, refreshHeader());
+                            setCookie("accessToken", accessToken);
+                            error.config.headers.Authorization = `Bearer ${accessToken}`;
+                            holder.current.resolve();
+                            return query.fetch();
+                        } catch (error) {
+                            holder.current.reject();
+                            deleteCookie("accessToken");
+                            deleteCookie("refreshToken");
+                            router.replace("/signin");
+                            return;
+                        }
+                    },
+                }),
+            })
+    );
+
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
